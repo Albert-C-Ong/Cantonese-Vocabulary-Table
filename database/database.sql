e534da040e1c9006dfe5650e1d8f3248ecf648c1
Albert-C-Ong <albert_ong_us@yahoo.com>
2021-09-22 17:47:34 -0700

    Vocabulary update

--- database/database.sql
+++ database/database.sql
@@ -3,26 +3,10 @@ SELECT * FROM vocabulary
 WHERE subcategory2 IS "States" AND length(chinese) > 1
 ORDER BY english;
 
-/*
-with r_SomeTable
-as
-(
-select *
-, row_number() over(partition by IDCol order by ValueCol) as rnk
-from SomeTable
-)
-update r_SomeTable
-set RANKCol = rnk
-*/
-
-SELECT ROW_NUMBER() OVER(ORDER BY english) AS row_num, *
-FROM vocabulary
-WHERE subcategory2 IS "States" AND length(chinese) > 1;
 
 -- (chinese, chinese_variation, jyutping, pinyin, english, category, subcategory, subcategory2, priority)
 -- ("", NULL, "", "", "", "", "", NULL, NULL), 
 INSERT INTO vocabulary VALUES
-  ("", NULL, "", "", "", "", "", NULL, NULL), 
   ("", NULL, "", "", "", "", "", NULL, NULL), 
   ("", NULL, "", "", "", "", "", NULL, NULL)
 ;
