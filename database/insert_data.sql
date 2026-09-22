ecc1694c31e293d69712bd871bc808ea778bdba3
Albert Ong <albert_ong_us@yahoo.com>
2026-09-21 19:58:29 -0700

    added vocabulary scraper

--- database/insert_data.sql
+++ database/insert_data.sql
@@ -3,8 +3,23 @@
 -- ("", NULL, "", "", "", "", "", NULL, NULL), 
 
 INSERT INTO vocabulary VALUES
-("", NULL, "", "", "", "", "", NULL, NULL), 
-("", NULL, "", "", "", "", "", NULL, NULL), 
-("", NULL, "", "", "", "", "", NULL, NULL), 
-("", NULL, "", "", "", "", "", NULL, NULL)
+('診所', NULL, 'can2 so2', 'zhěn suǒ', 'clinic, dispensary', 'Nouns', 'Healthcare', NULL, NULL), 
+('傷口', NULL, 'soeng1 hau2', 'shāng kǒu', 'wound, cut', 'Nouns', 'Healthcare', NULL, NULL), 
+('上門', NULL, 'soeng5 mun4', 'shàng mén', 'to drop in, to visit', 'Verbs', NULL, NULL, NULL), 
+('觸發', NULL, 'zuk1 faat3', 'chù fā', 'to trigger, to stir up, to activate', 'Verbs', NULL, NULL, NULL), 
+('警鐘', NULL, 'ging2 zung1', 'jǐng zhōng', 'alarm bell, warning', 'Nouns', 'Time', NULL, NULL), 
+('發生', NULL, 'fat2 sang1', 'fā shēng', 'to happen, to occur, to take place', 'Verbs', NULL, NULL, NULL), 
+('磅', NULL, 'bong6', 'bàng', 'pound (unit of weight)', 'Nouns', 'Measurements', NULL, NULL), 
+('兌現', NULL, 'deoi3 jin6', 'duì xiàn', 'to cash', 'Verbs', 'Commerce', NULL, NULL), 
+('租金', NULL, 'zou1 gam1', 'zū jīn', 'rent', 'Nouns', 'Economics', 'Finance', NULL), 
+('分析', NULL, 'fan1 sik1', 'fēn xī', 'to analyze, to review', 'Nouns', NULL, NULL, NULL), 
+('學習者', NULL, 'hok6 zaap6 ze2', 'xué xí zhě', 'learner, student', 'Nouns', 'Education', NULL, NULL), 
+('頭盔', NULL, 'tau4 kwai1', 'tóu kuī', 'helmet', 'Nouns', 'Fashion', NULL, NULL), 
+('表弟', NULL, 'biu2 dai6', 'biǎo dì', 'cousin', 'Nouns', 'Family and Friends', NULL, NULL), 
+('表哥', NULL, 'biu2 go1', 'biǎo gē', 'cousin', 'Nouns', 'Family and Friends', NULL, NULL), 
+('探', NULL, 'taam3', 'tàn', 'to explore, to search out, to scout, to visit', 'Verbs', NULL, NULL, NULL), 
+('油鍋', NULL, 'jau4 wo1', 'yóu guō', 'wok or pan containing hot oil', 'Nouns', 'Kitchenware', NULL, NULL), 
+('產生', NULL, 'caan2 sang1', 'chǎn shēng', 'to produce, to come into being, to form, to generate', 'Verbs', NULL, NULL, NULL), 
+('洗手盆', NULL, 'sai2 sau2 pun4', 'xǐ shǒu pén', 'sink, washbasin', 'Nouns', 'Bathroom', NULL, NULL), 
+('當事', NULL, 'dong1 si6', 'dāng shì', 'the matter directly concerned', 'Nouns', NULL,	NULL,	NULL)
 ;
